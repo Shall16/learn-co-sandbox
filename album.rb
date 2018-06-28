62f66e19e7c479f4d10cc146d5e4b4af52b3674b
Shall16 <shall130@rsdmo.org>
2018-06-28 23:38:42 +0000

    automatically backed up by learn

---
+++ album.rb
@@ -0,0 +1,17 @@
+class Album
+  def initialize(name,date)
+    @name = name
+    @date = date
+end 
+
+def name_of_album
+  @name
+end 
+def date
+  @date 
+end 
+end 
+
+
+
+shawn_mendes= Album.new("shawn_mendes","2018")
